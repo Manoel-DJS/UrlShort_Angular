ff7590df2e3af233747428ae4ebe469d80624cfa
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2025-04-10 21:17:39 -0300

    add new components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { InputTextApiComponent } from "./components/input-text-api/input-text-api.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, InputTextApiComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
