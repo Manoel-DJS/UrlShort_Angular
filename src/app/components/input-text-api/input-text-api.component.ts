5bec39a85c6ed9974e7121e7966e7b2ff0e058b7
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2025-04-20 13:42:15 -0300

    refactor method - alter setTimeout to timer RXJS

--- src/app/components/input-text-api/input-text-api.component.ts
+++ src/app/components/input-text-api/input-text-api.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { firstValueFrom, lastValueFrom } from 'rxjs';
+import { timer } from 'rxjs';
 import { ApiResponseService } from '../../services/api-response.service';
 import { ShortenedUrlResponse } from '../../models/ShortenedUrlResponse';
 
@@ -46,9 +46,7 @@ export class InputTextApiComponent {
     navigator.clipboard.writeText(text).then(() => {
       this.copied = true;
       console.log('Erro ao copiar:', this.apiResponse?.url)
-      setTimeout(() => {
-        this.copied = false;
-      }, 2000);
+      timer(2000).subscribe(() => this.copied = false);
     }).catch(err => {
       console.error('Erro ao copiar:', err);
       this.errorMessage = 'Não foi possível copiar a URL';
@@ -56,54 +54,3 @@ export class InputTextApiComponent {
   }
 
 }
-
-/** 
-
-  userInput: string = '';
-  apiResponse: any; // substitua "YourExpectedType" pelo tipo real esperado
-  errorMessage: string = '';
-  isLoading: boolean = false;
-
-  constructor(private http: HttpClient) { }
-
-  async sendToApi() {
-    if (!this.userInput.trim()) {
-      this.errorMessage = 'Por favor, digite algo antes de enviar';
-      return;
-    }
-
-    this.isLoading = true;
-    this.errorMessage = '';
-    this.apiResponse = null;
-
-    try {
-      // Substitua pela URL da sua API real
-      // const apiUrl = 'http://localhost:8080/shorten-url';
-      const apiUrl = 'https://encurtador-url-8lqz.onrender.com/shorten-url';
-      //this.apiResponse = await this.http.post(apiUrl, { url: this.userInput}).toPromise();
-      this.apiResponse = await firstValueFrom(
-        this.http.post(apiUrl, { url: this.userInput })
-      );
-    } catch (error) {
-      console.error('Erro na requisição:', error);
-      this.errorMessage = 'Ocorreu um erro ao chamar a API';
-    } finally {
-      this.isLoading = false;
-    }
-  }
-
-  copied = false;
-  copyToClipboard(text: string) {
-    navigator.clipboard.writeText(text).then(() => {
-      this.copied = true;
-
-      setTimeout(() => {
-        this.copied = false;
-      }, 2000);
-    }).catch(err => {
-      console.error('Erro ao copiar:', err);
-      this.errorMessage = 'Não foi possível copiar a URL';
-    });
-  }
-
-**/
